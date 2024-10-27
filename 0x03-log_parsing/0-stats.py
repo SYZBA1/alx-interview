fe28092f7043d2aab7190eb919c6ef3d5f63c34f
SYZBA1 <samyos9090@gmail.com>
2024-10-27 18:29:40 +0000

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,49 @@
 #!/usr/bin/python3
 
-""" script that reads stdin line by line and computes metrics """
+""" Script that reads stdin line by line and computes metrics """
 
 import sys
+import re
 
+def print_stats(status_counts, total_size):
+    """Prints information about the file size and status counts."""
+    print("File size: {}".format(total_size))
+    for code in sorted(status_counts.keys()):
+        if status_counts[code] > 0:
+            print("{}: {}".format(code, status_counts[code]))
 
-def printsts(dic, size):
-    """ WWPrints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+# Initialize the status code dictionary and counters
+status_counts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+total_size = 0
+line_count = 0
 
-
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
+# Regex to match the expected log format
+log_pattern = re.compile(r'^(?P<ip>[\d\.]+) - \[(?P<date>[^\]]+)\] "(?P<request>GET [^"]+)" (?P<status_code>\d{3}) (?P<file_size>\d+)$')
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
+        line_count += 1
 
-        stlist = line.split()
-        count += 1
+        match = log_pattern.match(line)
+        if match:
+            status_code = match.group('status_code')
+            file_size = int(match.group('file_size'))
 
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
+            # Update total file size
+            total_size += file_size
+            
+            # Update status code count if it's one of the specified codes
+            if status_code in status_counts:
+                status_counts[status_code] += 1
 
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
-
+        # Print stats after every 10 lines
+        if line_count % 10 == 0:
+            print_stats(status_counts, total_size)
 
 except KeyboardInterrupt:
-    printsts(sts, size)
-    raise
+    print_stats(status_counts, total_size)
+    sys.exit()
+
+# Final output after the end of input
+print_stats(status_counts, total_size)
+
