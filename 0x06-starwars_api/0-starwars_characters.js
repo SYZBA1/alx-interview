d8cb33fbf2380071e1dbcc79209db9479ef09c38
SYZBA1 <samyos9090@gmail.com>
2024-11-14 19:01:10 -0800

    fixed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,25 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
-    }
-  });
-};
+const endpoint = 'https://swapi-api.hbtn.io/api';
+const filmId = process.argv[2];
 
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
+request(`${endpoint}/films/${filmId}/`, async function (error, response, body) {
+  if (error) return console.log(error);
+
+  let characters = JSON.parse(body).characters;
+
+  for (const character of characters) {
+    await new Promise((resolve, reject) => {
+      request(character, (error, response, body) => {
+        if (error) {
+          reject(error); 
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve(body);
+        }
+      });
+    });
   }
-);
+:x
+:x
